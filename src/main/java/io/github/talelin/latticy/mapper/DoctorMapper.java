7b07ec6037133122da253c6bf9f4e47c1d21df17
cml <17753912735@163.com>
2024-11-26 11:27:55 +0800

    test1

---
+++ src/main/java/io/github/talelin/latticy/mapper/DoctorMapper.java
@@ -0,0 +1,50 @@
+package io.github.talelin.latticy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.talelin.latticy.model.DoctorDO;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 医师mapper接口
+ */
+@Repository
+public interface DoctorMapper extends BaseMapper<DoctorDO> {
+
+    /**
+     * 根据医生姓名查询医生
+     * @param name 医生姓名
+     * @return 医生数据对象列表
+     */
+    List<DoctorDO> selectByName(@Param("name") String name);
+
+    /**
+     * 根据医院查询医生
+     * @param hospital 医院名称
+     * @return 医生数据对象列表
+     */
+    List<DoctorDO> selectByHospital(@Param("hospital") String hospital);
+
+    /**
+     * 根据科室查询医生
+     * @param department 科室名称
+     * @return 医生数据对象列表
+     */
+    List<DoctorDO> selectByDepartment(@Param("department") String department);
+
+    /**
+     * 根据医生ID查询医生信息
+     * @param id 医生ID
+     * @return 医生数据对象
+     */
+    DoctorDO selectById(@Param("id") Integer id);
+
+    /**
+     * 根据医生姓名进行模糊查询
+     * @param q 查询关键字
+     * @return 医生数据对象列表
+     */
+    List<DoctorDO> selectByNameLikeKeyword(@Param("q") String q);
+}
